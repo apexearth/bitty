fd4c471c9276344d26977a371764d65140bf2223
cjacobs <apexearth@gmail.com>
2017-08-06 20:05:57 -0700

    Add some core classes, begin work on Plant.

--- src/game/entities/Plant/Trunk.js
+++ src/game/entities/Plant/Trunk.js
@@ -7,10 +7,21 @@ class Trunk {
             new Branch(),
             new Branch(),
         ]
+        this._sides    = this.createSides()
     }
 
     get branches() {
         return this._branches
+    }
+
+    createSides() {
+        let sides    = []
+        let count    = Math.floor(Math.random() * 10)
+        let position = Math.random() * Math.PI
+        let initialPosition = position
+        for (let i = 0; i < count; i++) {
+
+        }
     }
 }
 
