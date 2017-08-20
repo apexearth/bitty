96fdc9337602a7b8af5cfd4f8e7a67ff9d12cd91
Christopher Jacobs <apexearth@gmail.com>
2017-08-19 22:38:34 -0700

    move around branch code into Branch.js

--- src/game/entities/Plant/Branch.js
+++ src/game/entities/Plant/Branch.js
@@ -1,14 +1,55 @@
 class Branch {
     constructor({
-                    left, right, angle
+                    parent,
+                    children,
+                    descendants,
+                    length = 20
                 }) {
-        this.left  = left
-        this.right = right
-        this.angle = angle
+        this.length      = length + Math.random() * 20
+        this.angle       = parent.angle + Math.random() * Math.PI * .5 - Math.PI * .25
+        this.thickness   = .90 // fix me! I'm really a .5 to 1 value :)
+        this.branches    = []
+        this.descendants = descendants
+
+        for (let i = 0; i < children && descendants; i++) {
+            this.branches.push(new Branch({
+                parent     : this,
+                children   : 1 + Math.round(Math.random()),
+                descendants: descendants - 1
+            }))
+        }
+        return this
     }
 
-    draw(seconds, graphics) {
-        
+    draw(seconds, graphics, coords) {
+        // Force a point if no children (temporary)
+        if (this.branches.length === 0)
+            this.thickness = .5
+
+        let midpointLeft  = {
+            x: (coords.left.x * this.thickness + coords.right.x * (1 - this.thickness)),
+            y: (coords.left.y * this.thickness + coords.right.y * (1 - this.thickness))
+        }
+        let midpointRight = {
+            x: (coords.left.x * (1 - this.thickness) + coords.right.x * this.thickness),
+            y: (coords.left.y * (1 - this.thickness) + coords.right.y * this.thickness)
+        }
+        let endCoords     = {
+            left : {
+                x: midpointLeft.x + Math.cos(this.angle) * this.length,
+                y: midpointLeft.y + Math.sin(this.angle) * this.length
+            },
+            right: {
+                x: midpointRight.x + Math.cos(this.angle) * this.length,
+                y: midpointRight.y + Math.sin(this.angle) * this.length
+            }
+        }
+        graphics.moveTo(coords.left.x, coords.left.y)
+        graphics.lineTo(endCoords.left.x, endCoords.left.y)
+        graphics.moveTo(coords.right.x, coords.right.y)
+        graphics.lineTo(endCoords.right.x, endCoords.right.y)
+
+        this.branches.forEach(branch => branch.draw(seconds, graphics, endCoords))
     }
 }
 
--- src/game/entities/Plant/Trunk.js
+++ src/game/entities/Plant/Trunk.js
@@ -1,9 +1,10 @@
+const Branch = require('./Branch')
+
 class Trunk {
     constructor() {
         this.size   = 10 + Math.random() * 30
         this._sides = this.createSides()
     }
-
 
     get sides() {
         return this._sides
@@ -11,7 +12,7 @@ class Trunk {
 
     createSides() {
         let sides = []
-        let count = Math.floor(Math.random() * 53 + 5)
+        let count = Math.floor(Math.random() * 15 + 5)
         for (let i = 0; i < count; i++) {
             sides.push(this.createSide(count, i))
         }
@@ -24,7 +25,7 @@ class Trunk {
             right: Math.PI * 2 / count * (i + 1),
             angle: Math.PI * 2 / count * (i + .5), // Angle is the midpoint, representing direction.
         }
-        side.branch = this.createBranch(side)
+        side.branch = new Branch({parent: side, children: 2, descendants: Math.floor(2 + Math.random() * 5)})
         return side
     }
 
@@ -42,57 +43,9 @@ class Trunk {
             }
             graphics.moveTo(coords.left.x, coords.left.y)
             graphics.lineTo(coords.right.x, coords.right.y)
-            this.drawBranch(seconds, graphics, coords, side.branch)
+            side.branch.draw(seconds, graphics, coords, side.branch)
         }
         graphics.endFill()
-    }
-
-    createBranch(parent, children = 1, descendants = Math.floor(Math.random() * 5 + 5)) {
-        let branch = {
-            length   : 20 + Math.random() * 30,
-            angle    : parent.angle + Math.random() * Math.PI * .2 - Math.PI * .1,
-            thickness: .95, // fix me! I'm really a .5 to 1 value :)
-            branches : [],
-            descendants
-        }
-        for (let i = 0; i < children && descendants; i++) {
-            branch.branches.push(this.createBranch(branch, 1, descendants - 1))
-        }
-        return branch
-    }
-
-    drawBranch(seconds, graphics, coords, branch) {
-        // Force a point if no children (temporary)
-        if (branch.branches.length === 0)
-            branch.thickness = .5
-
-        branch.angle  = branch.angle + (1 - Math.random() * 2) * seconds
-        branch.length = branch.length + (1 - Math.random() * 2) * seconds
-
-        let midpointLeft  = {
-            x: (coords.left.x * branch.thickness + coords.right.x * (1 - branch.thickness)),
-            y: (coords.left.y * branch.thickness + coords.right.y * (1 - branch.thickness))
-        }
-        let midpointRight = {
-            x: (coords.left.x * (1 - branch.thickness) + coords.right.x * branch.thickness),
-            y: (coords.left.y * (1 - branch.thickness) + coords.right.y * branch.thickness)
-        }
-        let endCoords     = {
-            left : {
-                x: midpointLeft.x + Math.cos(branch.angle) * branch.length,
-                y: midpointLeft.y + Math.sin(branch.angle) * branch.length
-            },
-            right: {
-                x: midpointRight.x + Math.cos(branch.angle) * branch.length,
-                y: midpointRight.y + Math.sin(branch.angle) * branch.length
-            }
-        }
-        graphics.moveTo(coords.left.x, coords.left.y)
-        graphics.lineTo(endCoords.left.x, endCoords.left.y)
-        graphics.moveTo(coords.right.x, coords.right.y)
-        graphics.lineTo(endCoords.right.x, endCoords.right.y)
-
-        branch.branches.forEach(branch => this.drawBranch(seconds, graphics, endCoords, branch))
     }
 }
 
